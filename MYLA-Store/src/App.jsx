1951fdecd1716dd600fba57212c90acd7f737673
Pham Thanh <thanhpham28124@gmail.com>
2025-05-27 12:44:26 +0300

    create vite project

---
+++ MYLA-Store/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+         <h1 class="text-3xl font-bold underline text-center text-blue-500 bg-amber-400">Hello world!</h1>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
