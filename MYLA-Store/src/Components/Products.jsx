a518dae24f6ee32b24d895b41e94290c41b1455f
Pham Thanh <thanhpham28124@gmail.com>
2025-05-28 14:13:21 +0300

    change grid

--- MYLA-Store/src/Components/Products.jsx
+++ MYLA-Store/src/Components/Products.jsx
@@ -29,7 +29,7 @@ function Products() {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-10 p-5">
+      <div className="grid-container">
         {filteredProducts.map((product) => (
           <CardProduct key={product.id} product={product} onClick={openModal} />
         ))}
