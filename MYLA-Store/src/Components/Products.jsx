e9f38c73c72bc2db915b1d97c496561801cc1f8c
Pham Thanh <thanhpham28124@gmail.com>
2025-05-28 11:32:16 +0300

    gapgap

--- MYLA-Store/src/Components/Products.jsx
+++ MYLA-Store/src/Components/Products.jsx
@@ -28,7 +28,7 @@ function Products() {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-66 md:gap-10 p-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-10 p-5">
         {filteredProducts.map((product) => (
           <CardProduct key={product.id} product={product} onClick={openModal} />
         ))}
