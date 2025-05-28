61efadc834e5a02ad06f41c78ee9b3a5f69db95c
Pham Thanh <thanhpham28124@gmail.com>
2025-05-28 14:04:25 +0300

    change headerheader

--- MYLA-Store/src/Components/Header.jsx
+++ MYLA-Store/src/Components/Header.jsx
@@ -23,8 +23,8 @@ function Header() {
     <header className="bg-green-600 text-white shadow-lg sticky top-0 z-50">
       <div className="container mx-auto px-5 py-5 flex justify-between md:justify-around items-center">
         <div className="flex items-center space-x-4 tagesschrift-regular">
-          <FcShop size={50}/>
-          <h1 className="text-2xl md:text-3xl font-bold">MYLA Store</h1>
+          <FcShop size={30}/>
+          <h1 className="text-xl md:text-2xl font-bold">MYLA Store</h1>
         </div>
 
         <button
