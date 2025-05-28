11a40e6ab697154097fd9e2f89be66c86da13501
Pham Thanh <thanhpham28124@gmail.com>
2025-05-28 11:23:49 +0300

    fix padding picturepicture

--- MYLA-Store/src/Components/Products.jsx
+++ MYLA-Store/src/Components/Products.jsx
@@ -28,7 +28,7 @@ function Products() {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 p-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
         {filteredProducts.map((product) => (
           <CardProduct key={product.id} product={product} onClick={openModal} />
         ))}
