804424d3c80c2cd236ed4a07c63bf6092d281986
Pham Thanh <thanhpham28124@gmail.com>
2025-05-28 15:55:46 +0300

    finall version

--- MYLA-Store/src/Components/CardProduct.jsx
+++ MYLA-Store/src/Components/CardProduct.jsx
@@ -11,8 +11,8 @@ function CardProduct({ product, onClick }) {
         alt={product.name}
         className="w-full h-90 sm:h-90 object-fill rounded-lg mb-4"
       />
-      <h2 className="text-lg sm:text-2xl xl:text-3xl font-semibold text-gray-800 mb-5">{product.name}</h2>
-      <p className="text-green-600 font-medium text-lg sm:text-2xl xl:text-3xl">{product.price}</p>
+      <h2 className="text-lg sm:text-xl xl:text-2xl font-semibold text-gray-800 mb-5">{product.name}</h2>
+      <p className="text-green-600 font-medium text-lg sm:text-xl xl:text-2xl">{product.price}</p>
     </div>
   );
 }
