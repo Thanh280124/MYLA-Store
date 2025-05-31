cefb71ddbd2bec5ba51ebaa059ba72241be2aac7
Pham Thanh <thanhpham28124@gmail.com>
2025-05-31 10:12:48 +0300

    change gap

--- MYLA-Store/src/Components/CardProduct.jsx
+++ MYLA-Store/src/Components/CardProduct.jsx
@@ -7,7 +7,7 @@ function CardProduct({ product, onClick }) {
 
   return (
     <div
-      className="flex sm:flex-col justify-between items-center bg-white p-5 rounded-lg shadow-lg hover:shadow-xl transition transform hover:-translate-y-1 cursor-pointer"
+      className="flex sm:flex-col justify-between gap-2 items-center bg-white p-5 rounded-lg shadow-lg hover:shadow-xl transition transform hover:-translate-y-1 cursor-pointer"
       onClick={() => onClick(product)}
     >
       <img
@@ -15,7 +15,7 @@ function CardProduct({ product, onClick }) {
         alt={t(product.name)}
         className="w-45 h-35 sm:h-90 sm:w-full object-fill rounded-lg mb-4"
       />
-      <div className='flex flex-col justify-center items-center gap-10 sm:gap-2'>
+      <div className='flex flex-col justify-center items-center gap-3 sm:gap-2'>
       <h2 className="text-xl sm:text-2xl font-semibold text-gray-800 mb-5 text-center">{t(product.name)}</h2>
       <p className="text-xl sm:text-2xl inline-block bg-green-600 text-white font-semibold px-5 py-3 rounded-xl shadow-sm">{product.price}</p>
     </div>
